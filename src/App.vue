93a179117fabae45b30c97fec2746bed54f99ad3
Mario <netrix4@gmail.com>
2025-09-05 16:19:07 -0700

    First commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+import BlogPost from "./components/BlogPost.vue";
+import { ref } from "vue";
+
+const postList = ref([
+  { id: 1, title: "Mucho calor en Enseyork", content: "Contenido1" },
+  { id: 2, title: "titulo dosss" },
+  { id: 3, title: "titulo tres", content: "contenido de tarjeta tres" },
+  { id: 4 },
+]);
+console.log(postList);
+</script>
+<template>
+  <div class="card">
+    <!-- <BlogPost :id="1" title="Mucho calor en ensenada" />
+    <BlogPost :id="2" title="Mucho calor en ensenada" content="Esto es un contenido tambien" />
+    <BlogPost :id="3" content="Esto es un contenido tambien" />
+    <BlogPost :id="4" textColor="text-orange-500" />
+    <BlogPost :id="5" colorFondo="#000" /> -->
+    <BlogPost
+      v-for="post in postList"
+      :id="`${post.id}`"
+      :title="`${post.title}`"
+      :content="`${post.content}`"
+    />
+  </div>
+</template>
+<style lang="css">
+/* @import "tailwindcss"; */
+.card {
+  display: flex;
+  flex-direction: column;
+  padding-top: 5%;
+  padding-bottom: 5%;
+  padding-left: 15%;
+  padding-right: 15%;
+  gap: 1%;
+  background-color: #00ab67;
+  height: 100vh;
+  width: 100%;
+}
+</style>
---
+++ src/components/BlogPost.vue
@@ -0,0 +1,45 @@
+<script setup>
+// const props = defineProps(["title", "content", "textColor"]);
+const props = defineProps({
+  id: Number,
+  title: String,
+  content: { type: String, default: "Sin descripcion" },
+  textColor: String,
+  colorFondo: String,
+  colorText: String,
+});
+</script>
+<template>
+  <div class="card-body">
+    <h2 v-if="title" class="card-title">{{ title }}</h2>
+    <h2 v-else class="card-title">Esto es un titulo por defecto</h2>
+    <!-- <p v-if="content"> -->
+    <p>
+      {{ content }}
+    </p>
+    <!-- <p v-else>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, esse? Tenetur fuga minus
+      laborum cumque obcaecati! Magni nulla tenetur illum illo minima sed velit voluptatem suscipit
+      facilis, dolore doloremque veniam?
+    </p> -->
+  </div>
+</template>
+<style>
+/* @import "tailwindcss"; */
+
+.card-body {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 1%;
+  background: linear-gradient(270deg, #59848f, #00ccff);
+  border: 10px;
+  border-radius: 1%;
+  padding: 2%;
+  box-shadow: #00000025 10px 10px 20px;
+}
+.card-title {
+  font-weight: bold;
+  font-size: larger;
+}
+</style>
