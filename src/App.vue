21a0470360a418435985af3f02bf58996911fd18
Mario <netrix4@gmail.com>
2025-09-08 12:26:34 -0700

    ProductList array passed as a prop

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup>
-import BlogPost from "./components/BlogPost.vue";
 import { ref } from "vue";
+import BlogPost from "./components/BlogPost.vue";
 import TaskItem from "./components/TaskItem.vue";
+import ProductCard from "./components/ProductCard.vue";
 
 const postList = ref([
   { id: 1, title: "Mucho calor en Enseyork", content: "Contenido1" },
@@ -31,6 +32,29 @@ const taskList = ref([
     content: "Es agarrar practica con u proyecto personal",
   },
 ]);
+
+const productList = ref([
+  {
+    id: 1,
+    name: "Latop gamer",
+    price: 1299.99,
+    image: "Laptop.png",
+    description: "Laptop de alto rendimiento para gaming",
+  },
+  {
+    id: 2,
+    name: "Mac Mini (M2)",
+    price: 1399.01,
+    description: "Computadora de escritorio silenciosa con MacOS",
+  },
+  {
+    id: 3,
+    name: "Mouse Vertical",
+    price: 99.99,
+    image: "VerticalMouse.jpg",
+    description: "Mouse ergónomico para computadora",
+  },
+]);
 </script>
 <template>
   <div class="card">
@@ -45,20 +69,23 @@ const taskList = ref([
       :title="`${post.title}`"
       :content="`${post.content}`"
     /> -->
+    <!-- <TaskItem v-bind:task-list-prop="taskList" /> -->
 
-    <TaskItem v-bind:task-list-prop="taskList" />
+    <ProductCard :productList="productList" />
   </div>
 </template>
 <style lang="css">
 .card {
   display: flex;
   flex-direction: column;
+  overflow: auto;
   padding-top: 5%;
   padding-bottom: 5%;
   padding-left: 15%;
   padding-right: 15%;
-  gap: 1%;
+  gap: 5%;
   background-color: #00ab67;
-  height: 100vh;
+  /* height: 80vh; */
+  /* width: 100%; */
 }
 </style>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,40 @@
+<script setup>
+const componentProp = defineProps(["productList"]);
+</script>
+<template>
+  <div v-for="product in componentProp.productList">
+    <div class="main-container" :id="product.id">
+      <h2>{{ product.name }}</h2>
+      <h4>Precio: {{ product.price }}</h4>
+      <img
+        v-if="product?.image"
+        class="product-image"
+        :src="`/images/${product.image}`"
+      />
+      <p v-else>Imagen del producto no disponible</p>
+      <p>{{ product.description }}</p>
+    </div>
+  </div>
+</template>
+
+<style lang="css" scoped>
+.main-container {
+  font-size: large;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: rgb(189, 90, 90);
+  border: 10px;
+  border-radius: 10px;
+  padding: 3%;
+  margin: 1%;
+  color: #fff;
+}
+.product-image {
+  width: auto;
+  height: auto;
+  max-width: 200px;
+  max-height: 200px;
+  border-radius: 10%;
+}
+</style>
