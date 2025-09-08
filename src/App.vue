ae2fd0556adb82a57b634f92d087be8e67f1a2c6
Mario <netrix4@gmail.com>
2025-09-08 11:32:10 -0700

    Task array passed as a prop

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import BlogPost from "./components/BlogPost.vue";
 import { ref } from "vue";
+import TaskItem from "./components/TaskItem.vue";
 
 const postList = ref([
   { id: 1, title: "Mucho calor en Enseyork", content: "Contenido1" },
@@ -8,7 +9,28 @@ const postList = ref([
   { id: 3, title: "titulo tres", content: "contenido de tarjeta tres" },
   { id: 4 },
 ]);
-console.log(postList);
+
+const taskList = ref([
+  {
+    title: "Estudiar Vue",
+    content: "debo estudiar vue",
+    style: "background-color: #000;",
+  },
+  {
+    title: "Hacer ejercicios de componentes",
+    content: "Agarrar practica con componentes",
+    style: "background-color: #fac300;",
+  },
+  {
+    title: "Practicar props",
+    content: "mas practica para props",
+    style: "background-color: #000bab;",
+  },
+  {
+    title: "Crear proyecto personal",
+    content: "Es agarrar practica con u proyecto personal",
+  },
+]);
 </script>
 <template>
   <div class="card">
@@ -17,16 +39,17 @@ console.log(postList);
     <BlogPost :id="3" content="Esto es un contenido tambien" />
     <BlogPost :id="4" textColor="text-orange-500" />
     <BlogPost :id="5" colorFondo="#000" /> -->
-    <BlogPost
+    <!-- <BlogPost
       v-for="post in postList"
       :id="`${post.id}`"
       :title="`${post.title}`"
       :content="`${post.content}`"
-    />
+    /> -->
+
+    <TaskItem v-bind:task-list-prop="taskList" />
   </div>
 </template>
 <style lang="css">
-/* @import "tailwindcss"; */
 .card {
   display: flex;
   flex-direction: column;
@@ -34,11 +57,8 @@ console.log(postList);
   padding-bottom: 5%;
   padding-left: 15%;
   padding-right: 15%;
-  /* margin: 0%; */
   gap: 1%;
   background-color: #00ab67;
   height: 100vh;
-  /* overflow-y: auto; */
-  /* width: 100%; */
 }
 </style>
---
+++ src/components/TaskItem.vue
@@ -0,0 +1,28 @@
+<script setup>
+// const taskProp = defineProps(["taskTitle", "taskContent", "taskStyle"]);
+const taskList = defineProps(["taskListProp"]);
+</script>
+<template>
+  <div v-for="task in taskList.taskListProp">
+    <div class="main-container" :style="task.style">
+      <!-- <div :class="`main-container {${task.style}}`"> -->
+      <div class="task-item">
+        <h3>{{ task.title }}</h3>
+        <p>Contenidooo {{ task.content }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="css">
+.main-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  justify-items: center;
+  background-color: rgb(189, 90, 90);
+  border: 10px;
+  border-radius: 10px;
+  padding: 3%;
+  color: #fff;
+}
+</style>
